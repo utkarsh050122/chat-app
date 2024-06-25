5b69bc8cf2a9e040106a0d1566170685e4ffad8e
utkarsh050122 <utkrajshubham5122@gmail.com>
2024-06-25 08:24:01 +0530

    third commit1

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { AuthContextProvider } from "./context/AuthContext";
+import { ChatContextProvider } from "./context/Chatcontext";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+<AuthContextProvider>
+    <ChatContextProvider>
+      <App />
+    </ChatContextProvider>
+  </AuthContextProvider>
   </React.StrictMode>,
 )
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Chat from "../Component/Chat";
+import Sidebar from "../Component/Sidebar";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="container">
+        <Sidebar />
+        <Chat />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
